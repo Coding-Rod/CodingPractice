407699a81f3991de9c584eb0fab90d463905547d
Sergio Fernandez <sergiorodrigofernandeztesta@gmail.com>
2022-03-20 14:14:09 -0400

    validations_query

--- rodri/python/fastAPI/hello-world/main.py
+++ rodri/python/fastAPI/hello-world/main.py
@@ -7,6 +7,7 @@ from pydantic import BaseModel
 # FastAPI
 from fastapi import FastAPI
 from fastapi import Body
+from fastapi import Query
 
 app = FastAPI()
 
@@ -27,3 +28,15 @@ def home(): # Path operation function
 @app.post("/person/new")
 def create_person(person: Person = Body(...)) -> Person: # (...) = obligatorio
     return person
+
+# Validaciones: Query Parameters
+
+@app.get("/person/detail")
+def show_person(
+    name: Optional[str] = Query(None, min_length=1, max_length=50),
+    age: str = Query(...) 
+):
+    return {
+        "name": name,
+        "age": age
+    }
