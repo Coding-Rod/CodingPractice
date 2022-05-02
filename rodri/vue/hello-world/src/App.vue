efdc262ab25c2ae20c080ec95bba0442c8c35cb6
Rod5919 <sergiorodrigofernandeztesta@gmail.com>
2022-05-02 02:45:44 -0400

    sass and bem

--- rodri/vue/hello-world/src/App.vue
+++ rodri/vue/hello-world/src/App.vue
@@ -5,7 +5,7 @@
     <div class="dropdown">
       <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
         Dropdown button
-      </button    >
+      </button>
       <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
         <li><a class="dropdown-item" href="#">Action</a></li>
         <li><a class="dropdown-item" href="#">Another action</a></li>
--- rodri/vue/hello-world/src/components/HelloWorld.vue
+++ rodri/vue/hello-world/src/components/HelloWorld.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <ul class="list">
+      <li class="list_item_odd">item 1</li>
+      <li class="list_item_even">item 2</li>
+      <li class="list_item_odd">item 3</li>
+      <li class="list_item_even">item 4</li>
+    </ul>
   </div>
 </template>
 
@@ -14,7 +20,7 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style lang="scss">
 h3 {
   margin: 40px 0 0;
 }
@@ -28,5 +34,17 @@ li {
 }
 a {
   color: #42b983;
+}
+.list{
+  background-color: black;
+  &_item{
+    border: 1px solid red;
+    &_even{
+      color:white;
+    }
+    &_odd{
+      color: gray;
+    }
+  }
 }
 </style>
