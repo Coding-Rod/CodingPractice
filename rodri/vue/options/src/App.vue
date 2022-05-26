a350f1d9ce5af0b1bc323c5a49b7635c092116f2
Rod5919 <sergiorodrigofernandeztesta@gmail.com>
2022-05-26 11:46:46 -0400

    transitions

--- rodri/vue/options/src/App.vue
+++ rodri/vue/options/src/App.vue
@@ -1,22 +1,32 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <!-- <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Welcome to Your Vue.js App" /> -->
   <!-- <component :is="component_name"></component> -->
+  <button @click="show = !show">Menu</button>
+  <transition name="fade">
+    <MenuApp v-if="show" />
+  </transition>
 </template>
 
 <script>
+import MenuApp from "./components/MenuApp.vue";
 //#region imports
 // import HelloWorld from "./components/HelloWorld.vue";
-import { defineAsyncComponent } from "vue";
-const HelloWorld = defineAsyncComponent(() =>
-  import("./components/HelloWorld.vue")
-);
+// import { defineAsyncComponent } from "vue";
+// const HelloWorld = defineAsyncComponent(() =>
+//   import("./components/HelloWorld.vue")
+// );
 //#endregion imports
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    MenuApp,
+  },
+  data() {
+    return {
+      show: false,
+    };
   },
 };
 </script>
@@ -29,5 +39,15 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+
+.fade-leave-active,
+.fade-enter-active {
+  transition: opacity 0.5s ease;
 }
 </style>
---
+++ rodri/vue/options/src/components/MenuApp.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <ul>
+      <li>Opcion 1</li>
+      <li>Opcion 2</li>
+      <li>Opcion 3</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
