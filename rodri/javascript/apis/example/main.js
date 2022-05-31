9ecadaa9392c34f75c211af42d337542b89bf25d
Rod5919 <sergiorodrigofernandeztesta@gmail.com>
2022-05-30 21:58:39 -0400

    get three random cats

--- rodri/javascript/apis/example/main.js
+++ rodri/javascript/apis/example/main.js
@@ -1,10 +1,13 @@
 const URL = "https://api.thecatapi.com/v1/images/search";
-
+const limit = "3"
+const API_KEY = "cf5f08a7-26d8-42a4-9d4f-abbb4f0e87ba"
 const button = document.querySelector("#get_cat");
 
 button.addEventListener("click", (e) => {
     e.preventDefault();
-    reload();
+    for (let index = 0; index < 3; index++) {
+        reload_cat("#img"+(index+1));
+    }
 })
 
 // #region fetch
@@ -19,11 +22,17 @@ button.addEventListener("click", (e) => {
 // }
 // #endregion fetch
 
-const reload = async () => {
-    const response = await fetch(URL);
+const reload_cat = async (element) => {
+    const response = await fetch(
+                                URL+"?"
+                                +"limit="+limit
+                                +"&api_key="+API_KEY);
     const data = await response.json();
-    const img = document.querySelector("img");
-    img.src = data[0].url;;
+    const img = document.querySelector(element);
+    // console.log(response.status);
+    img.src = data[parseInt(element[element.length-1]-1)].url;
 }
 
-reload();
+reload_cat("#img1");
+reload_cat("#img2");
+reload_cat("#img3");
