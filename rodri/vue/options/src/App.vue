855409d37d21233484f7b3fe25ac55301e4a1fce
Rod5919 <sergiorodrigofernandeztesta@gmail.com>
2022-05-30 20:26:00 -0400

    Vue Modal

--- rodri/vue/options/src/App.vue
+++ rodri/vue/options/src/App.vue
@@ -1,27 +1,14 @@
 <template>
-  <!-- <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" /> -->
-  <!-- <component :is="component_name"></component> -->
-  <button @click="show = !show">Menu</button>
-  <transition name="fade">
-    <MenuApp v-if="show" />
-  </transition>
+  <modal>Show modal</modal>
 </template>
 
 <script>
-import MenuApp from "./components/MenuApp.vue";
-//#region imports
-// import HelloWorld from "./components/HelloWorld.vue";
-// import { defineAsyncComponent } from "vue";
-// const HelloWorld = defineAsyncComponent(() =>
-//   import("./components/HelloWorld.vue")
-// );
-//#endregion imports
+import ModalTemplate from "./components/ModalTemplate.vue";
 
 export default {
   name: "App",
   components: {
-    MenuApp,
+    modal: ModalTemplate,
   },
   data() {
     return {
---
+++ rodri/vue/options/src/components/ModalTemplate.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <button @click="toggle"><slot /></button>
+    <div v-show="show" class="modal">
+      <h1>Titulo</h1>
+      <p>Lorem ipsum dlor</p>
+      <button @click="toggle">Close</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      show: false,
+    };
+  },
+  methods: {
+    toggle() {
+      this.show = !this.show;
+    },
+  },
+};
+</script>
