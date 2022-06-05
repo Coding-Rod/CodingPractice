09b0d9863387407e7dacd8bec95b3aec8dbabbff
Rod5919 <sergiorodrigofernandeztesta@gmail.com>
2022-06-05 19:22:58 -0400

    movements

--- rodri/vue/cashflow/src/App.vue
+++ rodri/vue/cashflow/src/App.vue
@@ -28,3 +28,17 @@ export default {
   },
 };
 </script>
+
+<style>
+html,
+body,
+.app {
+  min-height: 100vh;
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+* {
+  --brand-green: #04b500;
+  --brand-blue: #0689b0;
+}
+</style>
