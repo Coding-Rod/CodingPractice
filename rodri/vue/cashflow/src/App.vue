7d4d0706bd8378c7e57472ec1f767c6ddc3eafac
Rod5919 <sergiorodrigofernandeztesta@gmail.com>
2022-06-04 21:58:05 -0400

    suspense

--- rodri/vue/cashflow/src/App.vue
+++ rodri/vue/cashflow/src/App.vue
@@ -1,13 +1,30 @@
 <template>
-  <Home></Home>
+  <Suspense>
+    <template #default>
+      <Home />
+    </template>
+    <template #fallback>
+      <SplashScreen />
+    </template>
+  </Suspense>
 </template>
 
 <script>
-import Home from "@/components/Home.vue";
+// import Home from "@/components/Home.vue";
+import SplashScreen from "@/components/SplashScreen.vue";
+import { defineAsyncComponent } from "vue";
 
 export default {
   components: {
-    Home,
+    SplashScreen,
+    Home: defineAsyncComponent(
+      () =>
+        new Promise((resolve) => {
+          setTimeout(() => {
+            resolve(import("@/components/Home.vue"));
+          }, 2500);
+        })
+    ),
   },
 };
 </script>
---
+++ rodri/vue/cashflow/src/components/SplashScreen.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="splashscreen">
+    <img src="@/assets/cashflow-icon.svg" alt="Logo" />
+    <h1><span>Cash</span>Flow</h1>
+  </div>
+</template>
+
+<style scoped>
+html,
+body,
+.app {
+  min-height: 100vh;
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+* {
+  --brand-green: #04b500;
+  --brand-blue: #0689b0;
+}
+.splashscreen {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 100vh;
+}
+h1 {
+  color: var(--brand-blue);
+}
+h1 span {
+  color: var(--brand-green);
+}
+</style>
