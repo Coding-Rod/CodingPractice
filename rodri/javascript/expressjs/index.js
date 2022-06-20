1d8aa75b4becc0afb7c5607d6a114b07b7868780
Rod5919 <sergiorodrigofernandeztesta@gmail.com>
2022-06-20 02:59:56 -0400

    cors

--- rodri/javascript/expressjs/index.js
+++ rodri/javascript/expressjs/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const routerApi = require('./routes/index.js');
 
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler.js');
@@ -7,6 +8,17 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
+
+const whitelist = ['http://localhost:5500'];
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin))
+      callback(null, true);
+    else
+      callback(new Error('Invalid origin'));
+  }
+}
+app.use(cors(options));
 
 app.get('/', (req, res) => {
   res.send("Hola mi server en express");
