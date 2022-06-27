e5413f3ee282c6e6e9735b88a963b0dfff4149d3
Rod5919 <sergiorodrigofernandeztesta@gmail.com>
2022-06-26 23:12:53 -0400

    responses

--- rodri/javascript/backend/server.js
+++ rodri/javascript/backend/server.js
@@ -1,17 +1,34 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const router = express.Router();
 
 var app = express();
 
+app.use(bodyParser.json());
 app.use(router);
 
 router.get("/message", (req, res) => {
+    console.log(req.headers);
+    res.header({
+        "custom-header": "custom-value"
+    });
     res.json({ message: "Hello World" });
 });
 
-router.post("/message", (req, res) => {
-    res.send("Message received");
+router.delete("/message", (req, res) => {
+    console.log(req.body);
+    console.log(req.query);
+    res
+    .status(201)
+    .send({
+        error: "",
+        message: "Message deleted"
+    });
 });
+
+// router.post("/message", (req, res) => {
+//     res.send("Message received");
+// });
 
 // app.use('/', (req, res) => {
 //     res.send('Hello World!');
