cca1f8bdaf544278be388ff798297b2dc560d484
Rod5919 <sergiorodrigofernandeztesta@gmail.com>
2022-06-26 22:51:59 -0400

    router

--- rodri/javascript/backend/server.js
+++ rodri/javascript/backend/server.js
@@ -1,10 +1,21 @@
 const express = require("express");
+const router = express.Router();
 
 var app = express();
 
-app.use('/', (req, res) => {
-    res.send('Hello World!');
+app.use(router);
+
+router.get("/message", (req, res) => {
+    res.json({ message: "Hello World" });
 });
+
+router.post("/message", (req, res) => {
+    res.send("Message received");
+});
+
+// app.use('/', (req, res) => {
+//     res.send('Hello World!');
+// });
 
 app.listen(3000);
 
