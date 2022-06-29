f90b4226c452eb61e6a9f7ab5e81daded7734ad3
ivansanguezax <ivansanguezax@gmail.com>
2022-06-28 20:48:17 -0400

    ivancito

--- Ivan/asincronismo/src/callback/index.js
+++ Ivan/asincronismo/src/callback/index.js
@@ -4,27 +4,18 @@ function sum(num1, num2) {
 
 function calc(num1, num2, callback) {
     return callback(num1, num2);
+};
+
+console.log(calc(2,2, sum));
+
+
+setTimeout(function(){
+    console.log('Hola JavaScript')
+},2000)
+
+
+function saludo(name){
+    console.log(`Hola ${name}`);
 }
 
-console.log(calc(6,2,sum));
-
-
-function date(callback){ //callback = printDate
-    // Time now
-    console.log(new Date); // Actual time (1)
-    
-    // After 3s 
-    setTimeout(function(){ //Process in 3s 
-        let date = new Date(); // date = Actual time "3 seconds passed" (3)
-        callback(date); // printDate(date);
-    }, 3000) // Configure time in ms
-
-    // After Time now
-    console.log("Middle process"); // Actual time (3)
-}
-
-function printDate(x){
-    console.log(x);
-}
-
-date(printDate);
+setTimeout(saludo, 2000, 'Ivan');
