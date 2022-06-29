79126d088999c4d05d0054f410ae9dcdff7d177a
Rod5919 <sergiorodrigofernandeztesta@gmail.com>
2022-06-28 23:34:07 -0400

    cors

--- rodri/javascript/backend/server.js
+++ rodri/javascript/backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const server = require("http").Server(app);
 
+const cors = require("cors");
 const bodyParser = require("body-parser");
 const socket = require("./utils/socket");
 const db = require('./data/db');
@@ -9,6 +10,8 @@ const router = require("./utils/routes");
 
 const uri = "mongodb+srv://db_user_platzi_nodejs:LPNuNiFxlq5SbRSc@cluster0.ul3ff.mongodb.net/telegram";
 db(uri);
+
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
