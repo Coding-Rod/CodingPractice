c38fe327d88d20cf808a99ec9f84cca54cb62ea0
Rod5919 <sergiorodrigofernandeztesta@gmail.com>
2022-07-21 23:41:22 -0400

    template manage

--- rodri/python/django/premiosplatzi/polls/views.py
+++ rodri/python/django/premiosplatzi/polls/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from .models import Question, Choice
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.all()
+    return render(request, 'polls/index.html', {'latest_question_list': latest_question_list})
+    # return HttpResponse(Question.objects.all())
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
