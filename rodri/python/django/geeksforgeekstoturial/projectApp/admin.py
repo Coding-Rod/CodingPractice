17688fd73c73c29df8d1cc36f130e02ac2e468cd
Rod5919 <sergiorodrigofernandeztesta@gmail.com>
2022-07-30 00:58:06 -0400

    POST images Succesfull

--- rodri/python/django/geeksforgeekstoturial/projectApp/admin.py
+++ rodri/python/django/geeksforgeekstoturial/projectApp/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from .models import GeeksModel
 
+@admin.register(GeeksModel)
 class GeeksModelAdmin(admin.ModelAdmin):
     fields = ['title', 'description', 'img']
     list_display = ['title', 'description', 'last_modified', 'img']
     list_filter = ['last_modified']
     search_fields = ["title", "description"]
 
-admin.site.register(GeeksModel, GeeksModelAdmin)
+
+admin.site.site_header = "My admin dashboard"
+# admin.site.register(GeeksModel, GeeksModelAdmin)
--- rodri/python/django/geeksforgeekstoturial/projectApp/models.py
+++ rodri/python/django/geeksforgeekstoturial/projectApp/models.py
@@ -4,13 +4,14 @@ from django.db import models
 
 # declare a new model with a name "GeeksModel"
 class GeeksModel(models.Model):
-		# fields of the model
-	title = models.CharField(max_length = 200)
-	description = models.TextField()
+	""" Custom form example from geeksforgeeks
+	Args:
+		models (models.Model): the model to be renamed.
+	"""	
+	title = models.CharField(max_length = 200, help_text = "Enter the title of the post")
+	description = models.TextField(help_text = "Enter the description of the post")
 	last_modified = models.DateTimeField(auto_now_add = True)
-	img = models.ImageField(upload_to = "images/")
+	img = models.ImageField(upload_to = "images/", help_text = "Upload the image of the post")
 
-		# renames the instances of the model
-		# with their title name
 	def __str__(self):
 		return self.title
