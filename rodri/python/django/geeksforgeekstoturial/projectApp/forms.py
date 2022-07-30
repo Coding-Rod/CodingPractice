4f203daeec7e620df908b3999384b1edae755c62
Rod5919 <sergiorodrigofernandeztesta@gmail.com>
2022-07-30 01:14:21 -0400

    custom form

--- rodri/python/django/geeksforgeekstoturial/projectApp/forms.py
+++ rodri/python/django/geeksforgeekstoturial/projectApp/forms.py
@@ -10,3 +10,12 @@ class GeeksForm(forms.ModelForm):
 	class Meta:
 		model = GeeksModel
 		fields = "__all__"
+
+class InputForm(forms.Form):
+   
+    first_name = forms.CharField(max_length = 200)
+    last_name = forms.CharField(max_length = 200)
+    roll_number = forms.IntegerField(
+                     help_text = "Enter 6 digit roll number"
+                     )
+    password = forms.CharField(widget = forms.PasswordInput())
--- rodri/python/django/geeksforgeekstoturial/projectApp/views.py
+++ rodri/python/django/geeksforgeekstoturial/projectApp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
-from .forms import GeeksForm
-from .models import GeeksModel
+from .forms import GeeksForm, InputForm
+from .models import GeeksModel, InputModel
 
 def index(request):
     return HttpResponse("Hello Geeks")
@@ -18,3 +18,15 @@ def home_view(request):
         else:
             print(form.errors.as_data())
     return render(request, "projectApp/home.html", context)
+
+def input_view(request):
+    context ={}
+    context['form']= InputForm()
+    if request.method == 'POST':
+        form = InputForm(request.POST)
+        if form.is_valid():
+            InputModel.objects.create(first_name = form.cleaned_data['first_name'], last_name = form.cleaned_data['last_name'], roll_number = form.cleaned_data['roll_number'], password = form.cleaned_data['password'])
+            return HttpResponseRedirect('/')
+        else:
+            print(form.errors.as_data())
+    return render(request, "projectApp/form1.html", context)
