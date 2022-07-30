1455748b35c1a1c4f7cd9e1a8565aadf58450a65
Rod5919 <sergiorodrigofernandeztesta@gmail.com>
2022-07-29 21:50:09 -0400

    forms

--- rodri/python/django/geeksforgeekstoturial/projectApp/urls.py
+++ rodri/python/django/geeksforgeekstoturial/projectApp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 #now import the views.py file into this code
 from . import views
 urlpatterns=[
-  path('',views.index)
+  path('',views.index),
+  path('home/',views.home_view),
 ]
--- rodri/python/django/geeksforgeekstoturial/projectApp/views.py
+++ rodri/python/django/geeksforgeekstoturial/projectApp/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .forms import InputForm
 
 def index(request):
     return HttpResponse("Hello Geeks")
 
+
+from django.shortcuts import render
+from .forms import InputForm
+ 
+# Create your views here.
+def home_view(request):
+    context ={}
+    context['form']= InputForm()
+    return render(request, "projectApp/home.html", context)
