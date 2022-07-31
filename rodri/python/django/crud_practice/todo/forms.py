32166a85286c354eea192ab5a3085e92f0848921
Rod5919 <sergiorodrigofernandeztesta@gmail.com>
2022-07-31 00:59:22 -0400

    Adding elements to list

--- rodri/python/django/crud_practice/todo/forms.py
+++ rodri/python/django/crud_practice/todo/forms.py
@@ -6,3 +6,7 @@ class TodoForm(forms.ModelForm):
     class Meta:
         model = Todo
         fields = "__all__"
+        widgets = {
+            "title": forms.TextInput(attrs={"type":"text", "class":"form-control", "id":"titleInput", "placeholder":"Title"}),
+            "description": forms.Textarea(attrs={"type":"text", "class":"form-control", "id":"descriptionInput", "placeholder":"Description", "rows":"3"}),
+        }
--- rodri/python/django/crud_practice/todo/views.py
+++ rodri/python/django/crud_practice/todo/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import Todo
+from .forms import TodoForm
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the todo index.")
+    item_list = Todo.objects.all()
+
+    form = TodoForm()
+    
+    if request.method == "POST":
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+        else:
+            print("error")
+            print(form.errors.as_data())    
+    context = {
+        "title": "TODO LIST",
+        "item_list": item_list,
+        "form": form
+    }
+    return render(request, 'todo/index.html', context)
