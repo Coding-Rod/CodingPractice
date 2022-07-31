a4fd75d1d4571dc6f1ebb5b47adf058a5cda49f3
Rod5919 <sergiorodrigofernandeztesta@gmail.com>
2022-07-31 17:10:24 -0400

    favicon

--- rodri/python/django/crud_practice/crud_practice/urls.py
+++ rodri/python/django/crud_practice/crud_practice/urls.py
@@ -14,10 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.conf.urls import include
+from django.urls import path, include
+from django.conf import settings
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('todo.urls')),
+    path(
+        "favicon.ico",
+        RedirectView.as_view(url="/static/favicon.ico"),
+    )
 ]
--- rodri/python/django/crud_practice/todo/tests.py
+++ rodri/python/django/crud_practice/todo/tests.py
@@ -1,3 +1,3 @@
 from django.test import TestCase
-
+from .models import Todo
 # Create your tests here.
