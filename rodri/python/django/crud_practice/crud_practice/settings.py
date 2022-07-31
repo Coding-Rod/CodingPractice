cac43ffd25eb86d03e5c19ff130846113f7a4b7a
Rod5919 <sergiorodrigofernandeztesta@gmail.com>
2022-07-30 22:21:41 -0400

    new tasks added from admin

--- rodri/python/django/crud_practice/crud_practice/settings.py
+++ rodri/python/django/crud_practice/crud_practice/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2#^&44j^+&c3jqxy8qa2&7l^rc5wfz2$!joi%#yy^felkd*x$7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.0.14']
 
 
 # Application definition
