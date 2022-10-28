08fac0516f4eeb421ba01f6b48164bdfb0edfdb5
Rod5919 <sergiorodrigofernandeztesta@gmail.com>
2022-10-28 07:09:45 -0400

    App readonly

--- rodri/typescript/ts-app/src/app/base.model.ts
+++ rodri/typescript/ts-app/src/app/base.model.ts
@@ -1,5 +1,5 @@
 export interface BaseModel {
-  id: string | number;
+  readonly id: string | number;
   createdAt: Date;
   updatedAt: Date;
 }
--- rodri/typescript/ts-app/src/app/products/product.service.ts
+++ rodri/typescript/ts-app/src/app/products/product.service.ts
@@ -3,5 +3,7 @@ import { Product } from './product.model';
 export const products: Product[] = [];
 
 export const addProduct = (data: Product):void => {
+  // data.id = "1"; // Error: Readonly property 'id' cannot be assigned to
+  data.createdAt = new Date(1999, 1, 1);
   products.push(data);
 }
