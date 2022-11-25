6c85b607d164c7608633fac44d2e801b65f169cf
Rod5919 <sergiorodrigofernandeztesta@gmail.com>
2022-11-25 11:11:56 -0400

    ngFor Objs

--- rodri/angular/my-store/src/app/app.component.ts
+++ rodri/angular/my-store/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Product } from './product.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -17,9 +17,43 @@ export class AppComponent {
     age: 22,
     img: 'https://source.unsplash.com/random',
   }
-  
+
   names: string[] = ['Sergio', 'Juan', 'Pedro', 'Maria'];
   newName = ''
+
+  products: Product[] = [
+    {
+      name: 'EL mejor juguete',
+      price: 565,
+      image: './assets/images/toy.jpg',
+      category: 'all',
+    },
+    {
+      name: 'Bicicleta casi nueva',
+      price: 356,
+      image: './assets/images/bike.jpg'
+    },
+    {
+      name: 'Colleción de albumnes',
+      price: 34,
+      image: './assets/images/album.jpg'
+    },
+    {
+      name: 'Mis libros',
+      price: 23,
+      image: './assets/images/books.jpg'
+    },
+    {
+      name: 'Casa para perro',
+      price: 34,
+      image: './assets/images/house.jpg'
+    },
+    {
+      name: 'Gafas',
+      price: 3434,
+      image: './assets/images/glasses.jpg'
+    }
+  ]
 
   toggleButton() {
     this.btnDisabled = !this.btnDisabled;
