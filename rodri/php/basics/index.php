92613e5a5d478d80771ee14f33dc95f372e3159f
Rod5919 <sergiorodrigofernandeztesta@gmail.com>
2022-11-30 10:35:27 -0400

    arrays

--- rodri/php/basics/index.php
+++ rodri/php/basics/index.php
@@ -10,4 +10,14 @@ echo "7Hello, my name is " . $name . " " . $lastname . "!\n";
 
 echo "The product of 2 and 3 is " . (2 * 3) . "!\n";
 
+$people = [
+    "Rodrigo" => 22,
+    "Juan" => 23,
+    "Pedro" => 24,
+];
+
+var_dump($people); // var_dump is a function that prints the type and value of a variable
+print_r($people); // print_r is a function that prints the value of a variable
+
+echo "\n";
 ?>
