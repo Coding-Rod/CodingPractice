e5959f944cbd96e824327194a514342f91887dc1
Rodrigo Fernandez <sergiorodrigofernandeztesta@gmail.com>
2022-12-05 10:18:35 -0400

    State

--- rodri/react/intro/environment/src/components/TodoSearch.js
+++ rodri/react/intro/environment/src/components/TodoSearch.js
@@ -2,16 +2,22 @@ import React from "react";
 import '../styles/TodoSearch.css';
 
 function TodoSearch() {
+    const [searchValue, setSearchValue] = React.useState('');
+
     const onSearchValueChange = (event) => {
         console.log(event.target.value);
+        setSearchValue(event.target.value);
     };
 
     return (
+        <React.Fragment>
         <input 
             className="TodoSearch" 
             placeholder="frontend" 
             onChange={onSearchValueChange}
         />
+        <p>{searchValue}</p>
+        </React.Fragment>
     );
 }
 
