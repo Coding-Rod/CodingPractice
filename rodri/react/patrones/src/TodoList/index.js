e887b5ec8cf9bf6db3895fc873bf2c4d535ea6ae
Rodrigo Fernandez <sergiorodrigofernandeztesta@gmail.com>
2022-12-16 01:19:30 -0400

    render function added

--- rodri/react/patrones/src/TodoList/index.js
+++ rodri/react/patrones/src/TodoList/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './TodoList.css'
 
 function TodoList(props) {
+  const renderFunc = props.children || props.render;
 
   return (
     <section className="TodoList">
@@ -9,10 +10,7 @@ function TodoList(props) {
       {props.loading && props.onLoading()} {/* This ocurr when the loading is true */}
       {!props.searchValue && !props.loading && !props.searchedTodos.length && props.onEmptyTodos()} {/* This ocurr when the loading is false and the searchedTodos is empty and there is no searchValue */}
       {props.searchValue && !props.loading && !props.searchedTodos.length && props.onEmptySearchResults( props.searchValue )} {/* This ocurr when the loading is false and the searchedTodos is empty and there is a searchValue */}
-      {props.searchedTodos.map(todo => props.render(todo))} {/* This ocurr when the loading is false and the searchedTodos is not empty */}
-      <ul>
-        {props.children}
-      </ul>
+      {props.searchedTodos.map(renderFunc)} {/* This ocurr when the loading is false and the searchedTodos is not empty */}
     </section>
   );
 }
