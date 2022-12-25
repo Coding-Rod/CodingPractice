897790a2eeb2a037d9775c6c804a9c0343b81a00
Rodrigo Fernandez <sergiorodrigofernandeztesta@gmail.com>
2022-12-25 19:36:46 -0400

    use effect with function

--- rodri/react/state/src/App.jsx
+++ rodri/react/state/src/App.jsx
@@ -1,15 +1,15 @@
+import React from 'react';
+
 import { UseState } from './components/UseState';
 import { ClassState } from './components/ClassState';
 
 import './styles/App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <UseState name="Use State"/>
-      <ClassState name="Class State"/>
-    </div>
-  );
+    return <div className="App">
+        <UseState name="Use State"/>
+        <ClassState name="Class State"/>
+    </div>;
 }
 
 export default App;
--- rodri/react/state/src/components/UseState.jsx
+++ rodri/react/state/src/components/UseState.jsx
@@ -2,19 +2,30 @@ import React from "react";
 
 const UseState = ({ name }) => {
     const [error, setError] = React.useState(false);
+    const [loading, setLoading] = React.useState(true);
+
+    React.useEffect(() => {
+        if (loading) {
+            setTimeout(() => {
+                setLoading(false);
+            }, 2000);
+        }
+    }, [loading]);
+
     return <div>
         <h2>Remove { name }</h2>
 
         <p>Please, write your security code:</p>
 
         { error && <p>Invalid security code</p> }
+        { loading && <p>Loading...</p> }
 
         <input type="text" name="securityCode" />
         <button
             onClick={() => {
                 setError(!error);
             }}
-            >Verify
+        >Verify
         </button>
     </div>;
 }
