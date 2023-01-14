af1737ede4912a0f624f6ba45229eca64b8e9363
Rodrigo Fernandez <sergiorodrigofernandeztesta@gmail.com>
2023-01-14 15:52:17 -0400

    watcher

--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -14,6 +14,7 @@ module.exports = {
         assetModuleFilename: 'assets/images/[hash][ext][query]',
     },
     mode: 'development',
+    watch: true,
     resolve: {
         extensions: ['.js'],
         alias: {
