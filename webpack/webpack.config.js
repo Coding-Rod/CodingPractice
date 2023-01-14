e678d7673ac6ecd28e93ab74fd47e81c36e76270
Rodrigo Fernandez <sergiorodrigofernandeztesta@gmail.com>
2023-01-14 15:37:20 -0400

    alias configuration

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -14,6 +14,12 @@ module.exports = {
     },
     resolve: {
         extensions: ['.js'],
+        alias: {
+            '@utils': path.resolve(__dirname, 'src/utils/'),
+            '@templates': path.resolve(__dirname, 'src/templates/'),
+            '@styles': path.resolve(__dirname, 'src/styles/'),
+            '@images': path.resolve(__dirname, 'src/assets/images/'),
+        },
     },
     module: {
         rules: [
@@ -41,7 +47,7 @@ module.exports = {
                         mimetype: 'application/font-woff',
                         name: '[name].[contenthash].[ext]',
                         outputPath: './assets/fonts/',
-                        publicPath: './assets/fonts/',
+                        publicPath: '../assets/fonts/',
                         esModule: false,
                     },
                 },
