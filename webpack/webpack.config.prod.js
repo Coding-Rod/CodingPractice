85d36beca4713919b1df80151ca55ab8e0892744
Rodrigo Fernandez <sergiorodrigofernandeztesta@gmail.com>
2023-01-14 15:47:05 -0400

    clean dist

--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -5,6 +5,7 @@ const CopyPlugin = require('copy-webpack-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -73,6 +74,7 @@ module.exports = {
             ],
         }),
         new Dotenv(),
+        new CleanWebpackPlugin(),
     ],
     optimization: {
         minimize: true,
