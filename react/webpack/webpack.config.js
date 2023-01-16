7f55ee806e973484a0974885dc9c9fe9173fbf22
Rodrigo Fernandez <sergiorodrigofernandeztesta@gmail.com>
2023-01-16 10:16:33 -0400

    :construction: [ADD] Webpack

---
+++ react/webpack/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                }
+            }
+        ]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 3006
+    }
+};
