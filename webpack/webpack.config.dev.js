275603ec747322876947008de77b83ea3fbdb9b2
Rodrigo Fernandez <sergiorodrigofernandeztesta@gmail.com>
2023-01-15 22:34:29 -0400

    :art: [ADD] Webpack server

--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -14,7 +14,6 @@ module.exports = {
         assetModuleFilename: 'assets/images/[hash][ext][query]',
     },
     mode: 'development',
-    watch: true,
     resolve: {
         extensions: ['.js'],
         alias: {
@@ -76,4 +75,10 @@ module.exports = {
         }),
         new Dotenv(),
     ],
+    devServer: {
+        static: path.join(__dirname, 'dist'),
+        compress: true,
+        historyApiFallback: true,
+        port: 3006,
+    },
 };
