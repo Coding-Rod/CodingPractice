a9dd00a0ca57747f2ce3a0e1e6e5842ae12c2b50
Rodrigo Fernandez <sergiorodrigofernandeztesta@gmail.com>
2023-01-16 14:11:42 -0400

    :art: [ADD] Example products

--- javascript/node/my-store/index.js
+++ javascript/node/my-store/index.js
@@ -3,4 +3,25 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/test', (req, res) => res.send('Hello Test!'));
+app.get('/products', (req, res) => {
+  res.json([
+    {
+      id: 1,
+      name: 'Product 1',
+      price: 100,
+    },
+    {
+      id: 2,
+      name: 'Product 2',
+      price: 200,
+    },
+    {
+      id: 3,
+      name: 'Product 3',
+      price: 300,
+    },
+  ]);
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
