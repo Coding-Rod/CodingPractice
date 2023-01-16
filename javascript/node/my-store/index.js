6f6c0a28de28f33743090052245f07b0201d6048
Rodrigo Fernandez <sergiorodrigofernandeztesta@gmail.com>
2023-01-16 14:35:47 -0400

    :heavy_plus_sign: [ADD] Faker and more endpoints
    Full get endpoints

--- javascript/node/my-store/index.js
+++ javascript/node/my-store/index.js
@@ -1,27 +1,53 @@
 const express = require('express');
+const faker = require('faker');
 const app = express();
 const port = 3000;
+
+const product_gen = (size = 100) => {
+  const products = [];
+  for (let i = 0; i < size; i++) {
+    products.push({
+      id: i,
+      name: faker.commerce.productName(),
+      price: faker.commerce.price(),
+      image: faker.image.image(),
+    });
+  }
+  return products;
+};
 
 app.get('/', (req, res) => res.send('Hello World!'));
 app.get('/test', (req, res) => res.send('Hello Test!'));
 app.get('/products', (req, res) => {
-  res.json([
+  const { size } = req.query;
+  res.json(product_gen(size));
+});
+
+app.get('/products/:id', (req, res) => {
+  const { id } = req.params;
+  const product = product_gen(parseInt(id) + 1).find(
+    (p) => p.id === parseInt(id)
+  );
+  res.json(product);
+});
+
+app.get('/users', (req, res) => {
+  const { limit, offset } = req.query;
+  const users = [
     {
       id: 1,
-      name: 'Product 1',
-      price: 100,
+      name: 'User 1',
     },
     {
       id: 2,
-      name: 'Product 2',
-      price: 200,
+      name: 'User 2',
     },
     {
       id: 3,
-      name: 'Product 3',
-      price: 300,
+      name: 'User 3',
     },
-  ]);
+  ];
+  res.json(users.slice(offset, limit));
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
