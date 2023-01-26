c972b7cca1ff95d482b7cd3a8ad3b4806f56ebaa
Rodrigo Fernandez <sergiorodrigofernandeztesta@gmail.com>
2023-01-26 01:25:20 -0400

    :sparkles: [ADD] CRUD
    CRUD was added to user service

--- javascript/node/my-store/src/services/user.service.js
+++ javascript/node/my-store/src/services/user.service.js
@@ -1,32 +1,41 @@
-// const boom = require('@hapi/boom');
-
+const boom = require('@hapi/boom');
 const { models } = require('../libraries/sequelize');
 
 class UserService {
   constructor() {}
 
   async create(data) {
-    return data;
+    const user = await models.UserModel.create(data);
+    return user;
   }
 
   async find() {
-    const rows = await models.UserModel.findAll();
+    const rows = await (
+      await models.UserModel.findAll()
+    ).sort((a, b) => {
+      if (a.id > b.id) return 1;
+      if (a.id < b.id) return -1;
+      return 0;
+    });
     return rows;
   }
 
   async findOne(id) {
-    return { id };
+    const user = await models.UserModel.findByPk(id);
+    if (!user) throw boom.notFound('User not found');
+    return user;
   }
 
   async update(id, changes) {
-    return {
-      id,
-      changes,
-    };
+    const user = await this.findOne(id);
+    const updatedUser = await user.update(changes);
+    return updatedUser;
   }
 
   async delete(id) {
-    return { id };
+    const user = await this.findOne(id);
+    const deletedUser = await user.destroy();
+    return deletedUser;
   }
 }
 
