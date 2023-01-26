eb8af19f589a5401608cf2f084d923c9e9bde629
Rodrigo Fernandez <sergiorodrigofernandeztesta@gmail.com>
2023-01-26 01:24:42 -0400

    :recycle: [REFACTOR] Category -> User

--- javascript/node/my-store/src/routes/users.router.js
+++ javascript/node/my-store/src/routes/users.router.js
@@ -26,8 +26,8 @@ router.get(
   async (req, res, next) => {
     try {
       const { id } = req.params;
-      const category = await service.findOne(id);
-      res.json(category);
+      const user = await service.findOne(id);
+      res.json(user);
     } catch (error) {
       next(error);
     }
@@ -40,8 +40,8 @@ router.post(
   async (req, res, next) => {
     try {
       const body = req.body;
-      const newCategory = await service.create(body);
-      res.status(201).json(newCategory);
+      const newUser = await service.create(body);
+      res.status(201).json(newUser);
     } catch (error) {
       next(error);
     }
@@ -56,8 +56,8 @@ router.patch(
     try {
       const { id } = req.params;
       const body = req.body;
-      const category = await service.update(id, body);
-      res.json(category);
+      const user = await service.update(id, body);
+      res.json(user);
     } catch (error) {
       next(error);
     }
