546aa4c5d7c7ab630cd96694fc91bc6c0106d595
Rodrigo Fernandez <sergiorodrigofernandeztesta@gmail.com>
2023-01-27 06:58:27 -0400

    :card_file_box: [ALTER] table customers
    user_id connected to users with unique field set to true, created

---
+++ javascript/node/my-store/src/db/migrations/20230127104220-alter-customer-table.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const { DataTypes } = require('sequelize');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface) {
+    await queryInterface.addColumn('customers', 'user_id', {
+      field: 'user_id',
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      unique: true,
+      references: {
+        model: 'users',
+        key: 'id',
+      },
+      onUpdate: 'CASCADE',
+      onDelete: 'SET NULL',
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.removeColumn('customers', 'user_id');
+  },
+};
