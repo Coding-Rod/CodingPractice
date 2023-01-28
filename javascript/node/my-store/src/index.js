3bd734264159d380efd5dd506a64f78f2716698c
Rodrigo Fernandez <sergiorodrigofernandeztesta@gmail.com>
2023-01-28 07:34:51 -0400

    :wrench: [CONFIG] CORS

--- javascript/node/my-store/src/index.js
+++ javascript/node/my-store/src/index.js
@@ -14,24 +14,20 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-const whitelist = ['http://localhost:8080', 'https://myapp.co'];
-const options = {
-  origin: (origin, callback) => {
-    if (whitelist.includes(origin) || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('no permitido'));
-    }
-  },
-};
-app.use(cors(options));
+// const whitelist = ['http://localhost:8080', 'https://myapp.co'];
+// const options = {
+//   origin: (origin, callback) => {
+//     if (whitelist.includes(origin) || !origin) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('no permitido'));
+//     }
+//   },
+// };
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hola mi server en express');
-});
-
-app.get('/nueva-ruta', (req, res) => {
-  res.send('Hola, soy una nueva ruta');
 });
 
 routerApi(app);
