957126c4312e102f4c0e64071d7745db2ae806f2
Rodrigo Fernandez <sergiorodrigofernandeztesta@gmail.com>
2023-01-30 16:23:44 -0400

    :sparkles: [ADD] Flat

--- javascript/arrays/server/14-flat.js
+++ javascript/arrays/server/14-flat.js
@@ -1,1 +1,13 @@
+const matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
 
+// The flat() method creates a new array with all sub-array elements concatenated into it recursively up to the specified depth.
+const flat = matrix.flat(2);
+console.log(flat);
+
+// The flatMap() method first maps each element using a mapping function, then flattens the result into a new array. It is identical to a map followed by a flat of depth 1, but flatMap is often quite useful, as merging both into one method is slightly more efficient.
+const flatMap = matrix.flatMap((row) => row.map((cell) => cell * 2));
+console.log(flatMap);
