db2979a09c7d59e67c79cbbfd54bdf5bc0df032e
Rodrigo Fernandez <sergiorodrigofernandeztesta@gmail.com>
2023-01-30 16:04:52 -0400

    :sparkles: [ADD] includes

--- javascript/arrays/server/11-includes.js
+++ javascript/arrays/server/11-includes.js
@@ -1,1 +1,14 @@
+const pets = [ 'cat', 'dog', 'rat' ];
 
+// With for
+let includesCat = false;
+for (let i = 0; i < pets.length; i++) {
+    if (pets[i] === 'cat') {
+        includesCat = true;
+        break;
+    }
+}
+console.log(includesCat);
+
+// With includes
+console.log(pets.includes('cat'));
