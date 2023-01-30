9a243c170528c73a23141d0daab299e32280b818
Rodrigo Fernandez <sergiorodrigofernandeztesta@gmail.com>
2023-01-30 11:12:12 -0400

    :sparkles: [ADD] ForEach

--- javascript/arrays/server/01-for-each.js
+++ javascript/arrays/server/01-for-each.js
@@ -1,1 +1,8 @@
+const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
 
+for (let i = 0; i < letters.length; i++) {
+  const letter = letters[i];
+  console.log('for: ',letter);
+}
+
+letters.forEach((letter) => console.log('forEach: ', letter));
