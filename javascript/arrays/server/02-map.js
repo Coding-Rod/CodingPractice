341453d68413e5e6518cad5caa27f69505a1f0e1
Rodrigo Fernandez <sergiorodrigofernandeztesta@gmail.com>
2023-01-30 11:24:51 -0400

    :sparkles: [ADD] Map

--- javascript/arrays/server/02-map.js
+++ javascript/arrays/server/02-map.js
@@ -1,1 +1,9 @@
+// Map is a function that takes an array and a function and creates a new array from the results of calling the function on each element of the input array.
+// Map is inmutable, it does not modify the original array.
 
+const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
+
+const mappedLetters = letters.map((letter) => letter.toUpperCase());
+
+console.log('letters: ', letters);
+console.log('mappedLetters: ', mappedLetters);
