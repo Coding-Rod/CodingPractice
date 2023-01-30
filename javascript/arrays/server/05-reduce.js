a08f9015e51324933d98dd278a3f15d85ea477e5
Rodrigo Fernandez <sergiorodrigofernandeztesta@gmail.com>
2023-01-30 14:37:04 -0400

    :sparkles: [ADD] reduce

--- javascript/arrays/server/05-reduce.js
+++ javascript/arrays/server/05-reduce.js
@@ -1,1 +1,14 @@
+const totals = [10, 20, 30, 40, 50];
 
+// With for
+let sum = 0;
+for (let i = 0; i < totals.length; i++) {
+    sum += totals[i];
+}
+console.log('Sum: ', sum);
+
+// With reduce
+// reduce takes 2 arguments: a callback function and an initial value
+// the callback function takes 2 arguments: an accumulator and a currentValue
+const sumReduce = totals.reduce((accumulator, currentValue) => accumulator + currentValue);
+console.log('SumReduce: ', sumReduce);
